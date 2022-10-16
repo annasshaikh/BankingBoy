706c6751df2344be5bb9cb2508c30961591d4e32
Annas Shaikh <annasrizwan2003@gmail.com>
2022-10-16 22:51:38 +0500

    Update Code and Added Explanation File.
    1) Added Menu and Validation of Input
    2) added A Explanation.txt
    3) Added A Error in ReadMe.md

--- banking.java
+++ banking.java
@@ -1,10 +1,26 @@
 import java.util.Scanner;
 
-public class banking {
+import javax.sound.midi.Soundbank;
 
+public class banking {
+  
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-  
+    System.out.println("Welcome To BankBoy: \n" +
+                      "Press 1 to Create Account \n" +
+                      "Press 2 To View Details \n" +
+                      "Press 3 For Deposit \n" +
+                      "Press 4 For Withdrawl \n" + 
+                      "Press 5 For Transation \n" );    
+    int choice = -1;
+    // This Will Ask For Value Until 1 - 5 is input.
+    while(true){
+      System.out.print("------> ");
+      choice = input.nextInt();
+      if (choice < = 5 && choice > = 1)
+        break;
+      System.out.println("Wrong Ïnput");
+    }
   
   }
 }
